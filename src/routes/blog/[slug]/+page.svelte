e86a533caede90676d5902bd9a5a72f274e8c833
nbblk <nbblks@gmail.com>
2023-11-05 14:18:58 +0900

    Modify: delete dynamic routes

--- src/routes/blog/[slug]/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	/** @type {import('./$types').PageData} */
-	export let data;
-</script>
-
-<h1>{data.title}</h1>
-<div>{@html data.content}</div>
--- src/routes/project/[slug]/+page.svelte
+++
@@ -1,2 +0,0 @@
-<h1>Project</h1>
-<h1>this is my project page</h1>
