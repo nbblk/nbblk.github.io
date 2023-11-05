afd1e29ac5c270b20ce3192d6ca78030d4690ea0
nbblk <nbblks@gmail.com>
2023-11-05 13:37:24 +0900

    Style: scaffold navbar and about

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.css';
+</script>
+
+<nav class="w-full h-[50px] p-8 flex justify-center items-center text-lg gap-8">
+	<a href="/project">Project</a>
+	<a href="/blog">Blog</a>
+</nav>
+
+<slot />
---
+++ src/routes/project/[slug]/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Project</h1>
+<h1>this is my project page</h1>
