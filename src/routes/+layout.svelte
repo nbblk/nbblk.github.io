10a35624cfb18ddc14eecd51defe05f4f97e50e2
nbblk <nbblks@gmail.com>
2023-11-05 14:38:10 +0900

    Config: add base path prefixes to pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
 	import '../app.css';
+	import { base } from '$app/paths';
 </script>
 
 <nav class="w-full h-[50px] p-8 flex justify-center items-center text-lg gap-8">
-	<a href="/project">Project</a>
-	<a href="/blog">Blog</a>
+	<a href="{base}/project">Project</a>
+	<a href="{base}/blog">Blog</a>
 </nav>
 
 <slot />
