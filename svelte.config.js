b84d9eee33d9d88659e3d6a830cf6a75df25b378
nbblk <nbblks@gmail.com>
2023-11-13 14:41:24 +0900

    Config: update svelte config to read process.env

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: '404.html'
+		}),
 		paths: {
-			base: '' || '/nbblk.github.io'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
