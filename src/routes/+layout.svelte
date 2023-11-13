8f8061a12cd4784b0ea3c1e1035204de5b324ddf
nbblk <nbblks@gmail.com>
2023-11-13 22:00:21 +0900

    Modify: add medium link to blog menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 
 <nav class="w-full h-[50px] p-8 flex justify-center items-center text-lg gap-8">
 	<a href="{base}/project">Project</a>
-	<a href="{base}/blog">Blog</a>
+	<a href="https://medium.com/@nbblks">Blog</a>
 </nav>
 
 <slot />
