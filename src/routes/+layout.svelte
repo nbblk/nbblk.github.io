ad4e278b91c681da6e0fd610bb9cef6d0fa08460
nbblk <nbblks@gmail.com>
2024-05-28 12:41:50 -0700

    modify: comment out project

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 
 <nav class="w-full h-[50px] p-8 flex justify-center items-center text-lg gap-8">
 	<a href="{base}/">About</a>
-	<a href="{base}/project">Project</a>
+	<!-- <a href="{base}/project">Project</a> -->
 	<a href="https://medium.com/@nbblks">Blog</a>
 	<a href="{base}/Blake_Sim_Resume.pdf">Resume</a>
 </nav>
