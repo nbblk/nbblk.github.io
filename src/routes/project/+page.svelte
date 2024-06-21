0e2c1834297f381569f0fdfe38c39e798ed4f74a
nbblk <nbblks@gmail.com>
2024-06-21 16:26:32 -0700

    feat: add project item in /project

--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -1,1 +1,13 @@
-<section class="w-full flex justify-center itens-center">working on it..</section>
+<section class="w-full flex justify-center items-center">
+	<ul class="w-full mt-[50px] flex flex-wrap items-center justify-center gap-8">
+		<li class="w-full flex justify-between gap-4">
+			<a href="https://vipassana-39orsf0ey-nbblks-projects.vercel.app" class="hover:opacity-50">
+				<img src="vipassana.png" alt="vipassana" />
+			</a>
+			<p>
+				Vipassana is a meditation app that helps users track their meditation sessions and provides
+				insights into their practice. (ongoing)
+			</p>
+		</li>
+	</ul>
+</section>
