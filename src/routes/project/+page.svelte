f10149e068f60e6549976f47781a527c55bd1dfb
nbblk <nbblks@gmail.com>
2024-06-21 16:30:23 -0700

    fix: project url

--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -1,7 +1,7 @@
 <section class="w-full flex justify-center items-center">
 	<ul class="w-full mt-[50px] flex flex-wrap items-center justify-center gap-8">
 		<li class="w-full flex justify-between gap-4">
-			<a href="https://vipassana-39orsf0ey-nbblks-projects.vercel.app" class="hover:opacity-50">
+			<a href="https://vipassana-chi.vercel.app" class="hover:opacity-50">
 				<img src="vipassana.png" alt="vipassana" />
 			</a>
 			<p>
