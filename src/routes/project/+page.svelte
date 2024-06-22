18add9aec4078f58f3d801ca293f304c73bb4d1d
nbblk <nbblks@gmail.com>
2024-06-21 17:04:05 -0700

    style: flex direction and padding

--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -1,10 +1,10 @@
 <section class="w-full flex justify-center items-center">
 	<ul class="w-full mt-[50px] flex flex-wrap items-center justify-center gap-8">
-		<li class="w-full flex justify-between gap-4">
+		<li class="w-full flex flex-col md:flex-row justify-between gap-4">
 			<a href="https://vipassana-chi.vercel.app" class="hover:opacity-50">
 				<img src="vipassana.png" alt="vipassana" />
 			</a>
-			<p>
+			<p class="p-4 md:p-0">
 				Vipassana is a meditation app that helps users track their meditation sessions and provides
 				insights into their practice. (ongoing)
 			</p>
